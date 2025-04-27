057c237b887840b1dbb6d3d42c258a829a1defc9
nadeer007 <nadeer007@gmail.com>
2025-04-27 15:17:07 +0530

    latest

--- src/components/includes/ProductCard.tsx
+++ src/components/includes/ProductCard.tsx
@@ -45,7 +45,7 @@ function ProductCard({ className, data, border = true, MoreItems = false }: Prod
     className={cn(
         'hover:opacity-[.9] max-sm:shadow-lg transition-all transform px-[16px] py-[12px] rounded-[4px] flex flex-col gap-3 items-center  max-md:px-[10px] max-md:py-[8px] ',
         border ? 'border-[.5px] sm:border  border-[#C5CBD5]/50 border-solid' : '',
-        activeHover && 'border border-[#0457C8] border-solid hover:scale-[1.02] ', // Apply border conditionally
+        activeHover && 'border border-[#0457C8] border-solid ', // Apply border conditionally
         className
     )}
 >
