db830805eeb32aeb24987582b65e2ccedd6d1635
nadeer007 <nadeer007@gmail.com>
2025-05-04 12:41:23 +0530

    latest

--- src/components/includes/RoundSection.tsx
+++ src/components/includes/RoundSection.tsx
@@ -8,13 +8,13 @@ import Link from 'next/link';
 
 export default function RoundSection({ sectionTitle,category=false, data }: any) {
   return (
-    <div className='mb-[28px] md:mb-[48px] bg-PRIMARY_GREY p-4 rounded-[6px]  ' >
+    <div className='mb-[28px] md:mb-[48px] bg-PRIMARY_GREY p-2 sm:p-4 rounded-[6px]  ' >
       <TitleSection sectionTitle={sectionTitle} />
-        <div className={cn('flex gap-[30px] max-md:gap-[20px] px-[10px] w-[100%]  no-scrollbar  items-baseline ',
+        <div className={cn('flex gap-[10px] sm:gap-[20px] md:gap-[30px] px-[10px] w-[100%]  no-scrollbar  items-baseline ',
         category ? 'overflow-x-scroll ' : 'overflow-scroll'
         )}>
         {data?.map((item:any, index:any) => (
-        <Link href={''} key={index} className={cn('items-center w-[122px] min-w-[122px] max-w-[122px] mb-2')}>
+        <Link href={''} key={index} className={cn('items-center w-[80px] min-w-[80px] max-w-[80px] sm:sw-[122px] sm:min-w-[122px] sm:max-w-[122px] mb-2')}>
           <RoundProduct category={category} image={ item?.icon} />
           { category && <div className='mt-[8px]'><h1 className='font-normal rubik_regular text-center sm:text-[16px] text-[14px] hover:text-[#0457C8] line-clamp-2'>{item?.name}</h1></div>}
         </Link>
